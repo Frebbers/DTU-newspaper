1e225f85b5d662886226bbb7db365874289d0f00
Nico Laursen <s235084@dtu.dk>
2024-03-25 12:20:33 +0100

    v

--- src/main/SQL/CreateDataBase.sql
+++ src/main/SQL/CreateDataBase.sql
@@ -69,5 +69,5 @@ CREATE TABLE Article
     Release_Date DATE         NOT NULL,
     Author_id    INT          NOT NULL,
     Title        VARCHAR(255) NOT NULL,
-    View_Count   INT DEFAULT 0,
+    View_Count   INT DEFAULT 0
 );
