2e369b6157cc3deb2a1268f929837e0eb142ca16
Zia Beg <ziabeg@hotmail.com>
2024-03-27 14:42:00 +0100

    SQL Programming done

---
+++ src/main/SQL/SQLProgramming.sql
@@ -0,0 +1,34 @@
+USE dkavisen;
+DROP PROCEDURE IF EXISTS calculatePeriodicity;
+DROP FUNCTION IF EXISTS countJournalists;
+DROP TRIGGER IF EXISTS triggerCalculatePeriodicity;
+DELIMITER //
+
+CREATE PROCEDURE calculatePeriodicity(IN vTitle varchar(255))
+BEGIN
+    DECLARE r1,r2 DATE;
+
+    SET r1 = (SELECT Release_Date FROM dkavisen.edition WHERE (Newspaper_Title = vTitle) ORDER BY Release_Date DESC LIMIT 1,1);
+    SET r2 = (SELECT Release_Date FROM dkavisen.edition WHERE (Newspaper_Title = vTitle) ORDER BY Release_Date DESC LIMIT 1);
+
+    UPDATE dkavisen.newspaper SET Periodicity = DATEDIFF(r2,r1) WHERE Title = vTitle;
+END //
+
+CREATE TRIGGER triggerCalculatePeriodicity
+AFTER INSERT ON dkavisen.edition FOR EACH ROW
+BEGIN
+    DECLARE latestEdition varchar(255);
+
+    SET latestEdition = (SELECT Newspaper_Title FROM dkavisen.edition WHERE Release_Date = (SELECT MAX(Release_date) FROM dkavisen.edition));
+
+    CALL calculatePeriodicity(latestEdition);
+END //
+
+CREATE FUNCTION countJournalists() RETURNS INT
+BEGIN
+    DECLARE vJourns INT;
+    SELECT COUNT(*) INTO vJourns FROM journalist;
+    RETURN vJourns;
+END //
+
+DELIMITER ;
