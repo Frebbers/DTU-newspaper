2130b9a853bc54f55dd2870515b4e2380b3b0d0d
Casper Kielstrup <casper.kielstrup@hotmail.com>
2024-03-29 11:59:11 +0100

    3. Querys answered. More data for each table prefered, 10-20. Querys specifically asks for Topics, for which we do not have a table/data

--- src/main/SQL/Queries.sql
+++ src/main/SQL/Queries.sql
@@ -1,2 +1,25 @@
 Use dkavisen;
 SELECT * from writes;
+
+SELECT j.First_name, j.Last_name, SUM(a.View_Count) as Total_Views
+FROM Journalist j
+JOIN Writes w ON j.CPR_NUMBER = w.journalist_id
+JOIN Article a ON w.article_id = a.ID
+GROUP BY j.CPR_NUMBER
+ORDER BY Total_Views DESC
+LIMIT 10;
+
+SELECT j.First_name, j.Last_name
+FROM journalist j
+JOIN reporter r ON j.CPR_NUMBER = r.Reporter_ID
+JOIN image i ON r.Image_ID = i.ID
+GROUP BY  j.CPR_NUMBER
+HAVING COUNT(r.Image_ID) = 1;
+
+SELECT DISTINCT j.First_name, j.Last_name
+FROM Journalist j
+JOIN Writes w ON j.CPR_NUMBER = w.journalist_id
+JOIN Article a ON w.article_id = a.ID
+JOIN ArticlePhotos ap ON a.ID = ap.Article_id
+JOIN Image i ON ap.img_id = i.ID
+JOIN Reporter r ON j.CPR_NUMBER = r.Reporter_ID;
