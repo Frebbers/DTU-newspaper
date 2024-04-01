afd712453650021603b29cfee2f967be128e2930
Frebbers <frederik.bode@hendrichsen.dk>
2024-04-01 13:47:14 +0200

    Created pathloader in LoaderExample.java

--- src/main/java/LoaderExample.java
+++ src/main/java/LoaderExample.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
@@ -6,9 +7,10 @@ public class LoaderExample {
 	public static void main(String[] args) {
 		PhotosAndReportersLoader loader = new PhotosAndReportersLoader();
 		try {
-			args[0] = "src/main/resources/photosAndReporters.csv";
-			System.out.println("loading from "+args[0]);
-			List<PhotoAndReporter> photosAndReporters = loader.loadPhotosAndReporters(args[0]);
+			//System.out.println("loading from "+args[0]);
+			String relativePath = "src/main/resources/uploads.csv";
+			String absolutePath = new File(relativePath).getAbsolutePath();
+			List<PhotoAndReporter> photosAndReporters = loader.loadPhotosAndReporters(absolutePath);
 			for(PhotoAndReporter photoAndReporter : photosAndReporters) {
 				System.out.print("\tPhoto: " + photoAndReporter.getPhoto());
 				System.out.println("\tReporter: " + photoAndReporter.getReporter());
