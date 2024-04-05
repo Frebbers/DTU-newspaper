80af550aa8dbd448f4faee47c4fc2e3ef461f71c
Casper Kielstrup <casper.kielstrup@hotmail.com>
2024-04-05 14:29:11 +0200

    moved sqlmodifications to script 2

--- src/main/SQL/21_02327DatabaseScript2_2024.sql
+++ src/main/SQL/21_02327DatabaseScript2_2024.sql
@@ -3,10 +3,29 @@
     (3) the statements used to create and apply functions, procedures, triggers, and
  */
 
+/* SQL Modifications */
+
+use dkavisen;
+
+INSERT INTO Article (Release_Date, Title, Edition_Title, Edition_Release_date, Topic, View_Count)
+VALUES ('2023-01-06', 'Article 11', 'The Times', '2023-01-01', 'Crime', 0);
+
+UPDATE Article
+SET View_Count = View_Count + 20
+WHERE Title = 'Article 1';
+
+
+SELECT * FROM editor WHERE Editor_ID = (SELECT CPR_NUMBER FROM journalist WHERE First_name = 'Jack' AND Last_name = 'Brown');
+DELETE FROM editor WHERE  Editor_ID = (SELECT CPR_NUMBER FROM journalist WHERE First_name = 'Jack' AND Last_name = 'Brown');
+SELECT * FROM journalistphonenumbers WHERE CPR_NUMBER = (SELECT  CPR_NUMBER FROM journalist WHERE First_name = 'Jack' AND Last_name = 'Brown');
+DELETE FROM journalistphonenumbers WHERE CPR_NUMBER = (SELECT  CPR_NUMBER FROM journalist WHERE First_name = 'Jack' AND Last_name = 'Brown');
+DELETE FROM reporter WHERE Reporter_id = (SELECT CPR_NUMBER FROM journalist WHERE First_name = 'Jack' AND Last_name = 'Brown');
+DELETE FROM writes WHERE journalist_CPR = (SELECT CPR_NUMBER FROM journalist WHERE First_name = 'Jack' AND Last_name = 'Brown');
+DELETE FROM Journalist
+WHERE First_name = 'Jack' AND Last_name = 'Brown';
 
 /* Queries */
 
-Use DKavisen;
 
 SELECT a.Topic,
        (SELECT a2.Title
