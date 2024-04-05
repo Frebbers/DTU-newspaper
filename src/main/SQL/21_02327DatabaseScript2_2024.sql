5ae5fa48e49d56505ba3110bb772cdfc5707f32a
Casper Kielstrup <casper.kielstrup@hotmail.com>
2024-04-05 14:30:46 +0200

    moved sqlprogramming to script 2

--- src/main/SQL/21_02327DatabaseScript2_2024.sql
+++ src/main/SQL/21_02327DatabaseScript2_2024.sql
@@ -1,11 +1,47 @@
-/*  (1) the delete/update statements used to change the tables (as in section 5), and
-    (2) the queries made (as in section 6), and
-    (3) the statements used to create and apply functions, procedures, triggers, and
- */
+/*  the statements used to create and apply functions, procedures, triggers */
+USE dkavisen;
+DROP PROCEDURE IF EXISTS calculatePeriodicity;
+DROP FUNCTION IF EXISTS countJournalists;
+DROP TRIGGER IF EXISTS triggerCalculatePeriodicity;
+DROP TRIGGER IF EXISTS Image_Before_Delete;
+DELIMITER //
+
+CREATE PROCEDURE calculatePeriodicity(IN vTitle varchar(255))
+BEGIN
+    DECLARE r1,r2 DATE;
+
+    SET r1 = (SELECT Release_Date FROM dkavisen.edition WHERE (Newspaper_Title = vTitle) ORDER BY Release_Date DESC LIMIT 1,1);
+    SET r2 = (SELECT Release_Date FROM dkavisen.edition WHERE (Newspaper_Title = vTitle) ORDER BY Release_Date DESC LIMIT 1);
+
+    UPDATE dkavisen.newspaper SET Periodicity = DATEDIFF(r2,r1) WHERE Title = vTitle;
+END //
+
+CREATE TRIGGER triggerCalculatePeriodicity
+    AFTER INSERT ON dkavisen.edition FOR EACH ROW
+BEGIN
+    DECLARE latestEdition varchar(255);
+
+    SET latestEdition = (SELECT Newspaper_Title FROM dkavisen.edition WHERE Release_Date = (SELECT MAX(Release_date) FROM dkavisen.edition));
+
+    CALL calculatePeriodicity(latestEdition);
+END //
+
+CREATE TRIGGER Image_After_Delete
+    AFTER DELETE ON dkavisen.reporter FOR EACH ROW
+BEGIN
+    UPDATE dkavisen.image SET Reporter_id = null WHERE Reporter_id = OLD.Reporter_ID;
+END //
+
+CREATE FUNCTION countJournalists() RETURNS INT
+BEGIN
+    DECLARE vJourns INT;
+    SELECT COUNT(*) INTO vJourns FROM journalist;
+    RETURN vJourns;
+END //
+
+DELIMITER ;
 
 /* SQL Modifications */
-
-use dkavisen;
 
 INSERT INTO Article (Release_Date, Title, Edition_Title, Edition_Release_date, Topic, View_Count)
 VALUES ('2023-01-06', 'Article 11', 'The Times', '2023-01-01', 'Crime', 0);
